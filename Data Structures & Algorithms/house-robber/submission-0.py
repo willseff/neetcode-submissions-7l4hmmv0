d388f82b78ce5922b40db2d3de6fd8426408f3d0
Will <32254701+willseff@users.noreply.github.com>
2026-04-11 11:06:54 -0400

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
+        if len(nums) == 2:
+            return max(nums[1], nums[0])
+        
+        # each step compute the max if house is robbed vs not robbed
+        # if house is robbed then we add that to the max of the house before if it wasn't robbed
+        # if house is not robbed then we add 0 to th emax of the house before if it was or was not robbed
+        i = 2
+        # cannot be robbed, could be robbed
+        dp = [nums[0], max(nums[1], nums[0])]
+        while i < len(nums):
+            print(i)
+            print(dp)
+            not_robbed = dp[1]
+            can_rob = max(nums[i] + dp[0], dp[1])
+
+            dp = [not_robbed, can_rob]
+
+            i += 1
+        
+        return max(dp)
