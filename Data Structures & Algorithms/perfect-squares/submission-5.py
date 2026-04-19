a28276267492f789a507470fcd8cdf5f890cf339
Will <32254701+willseff@users.noreply.github.com>
2026-04-19 13:31:41 -0400

    Add: perfect-squares - submission-5

---
+++ Data Structures & Algorithms/perfect-squares/submission-5.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(10000)
+class Solution:
+    def numSquares(self, n: int) -> int:
+        cache = {}
+        possibleSquaresCache = {}
+        def mem(n):
+            if n in cache:
+                return cache[n]
+
+            if n in possibleSquaresCache:
+                possibleSquares = possibleSquaresCache[n]
+
+            else:
+                possibleSquares = []
+                for i in range(n):
+                    if (i+1)**2 > n: 
+                        break
+                    possibleSquares.append((i+1)**2)
+                possibleSquaresCache[n] = possibleSquares
+            
+            if n in possibleSquares:
+                return 1
+
+            subtract = [mem(n - x) for x in possibleSquares]
+
+            cache[n] = min(subtract) + 1
+
+            return cache[n]
+
+        return mem(n)
+        
