4ee6d0b4f47401ee2603898e02c0dcd0c3367504
Will <32254701+willseff@users.noreply.github.com>
2026-05-06 20:49:14 -0400

    Add: maximum-product-subarray - submission-6

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-6.py
@@ -0,0 +1,21 @@
+import math
+sys.setrecursionlimit(2000)
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        def dfs(L, R, cache):
+            if (L, R) in cache:
+                return cache[(L, R)]
+            if L >= R:
+                cache[(L, R)] = -99999999999
+                return -99999999999
+
+            currProd = math.prod(nums[L: R])
+
+            cache[(L, R)] = max(currProd, dfs(L+1, R, cache), dfs(L, R-1, cache), dfs(L+1, R-1, cache))
+            return cache[(L, R)]
+
+        return dfs(0, len(nums), {})
+
+
+
+
