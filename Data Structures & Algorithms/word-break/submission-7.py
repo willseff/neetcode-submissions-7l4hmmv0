5cbfc4c500602b8b046113fa05471f0e2621eaff
Will <32254701+willseff@users.noreply.github.com>
2026-05-06 20:01:37 -0400

    Add: word-break - submission-7

---
+++ Data Structures & Algorithms/word-break/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def mem(s, wordDict, cache):
+            if s in cache:
+                return cache[s]
+            res = False
+            for i in range(len(s)+1):
+                if s[0:i] in wordDict:
+                    if i < len(s):
+                        res = res or mem(s[i:], wordDict, cache)
+                        cache[s] = res
+                    else:
+                        return True
+            return res
+            
+        return mem(s, wordDict, {})
+        
